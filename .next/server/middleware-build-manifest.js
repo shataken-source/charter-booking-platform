00c6fb81d77ac4fd7ab8ae96a3de4b77dec361a6
shataken-source <shataken@gmail.com>
2025-11-24 07:14:45 -0600

    gccq.02finalm

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,39 @@
-self.__BUILD_MANIFEST={"polyfillFiles":["static/chunks/polyfills.js"],"devFiles":["static/chunks/fallback/react-refresh.js"],"ampDevFiles":["static/chunks/fallback/webpack.js","static/chunks/fallback/amp.js"],"lowPriorityFiles":[],"rootMainFiles":[],"pages":{"/_app":["static/chunks/fallback/webpack.js","static/chunks/fallback/main.js","static/chunks/fallback/pages/_app.js"],"/_error":["static/chunks/fallback/webpack.js","static/chunks/fallback/main.js","static/chunks/fallback/pages/_error.js"]},"ampFirstPages":[]}
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ],
+    "/admin/campaigns/new": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/admin/campaigns/new.js"
+    ],
+    "/test-email": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/test-email.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
